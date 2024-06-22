81fd0a8a3555611b2612cfde21739eec74189939
mythdebunker <peceteamusic@gmail.com>
2024-06-22 17:35:04 +0300

    ad and mobile respo

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const misinfoList = document.getElementById('misinfo-list');
-    const toggleButton = document.getElementById('toggle-button');
-    const randomFactButton = document.getElementById('random-fact-button');
     const facts = Array.from(misinfoList.children);
 
     const colors = ['#ffe4e1', '#f0e68c', '#add8e6', '#90ee90', '#ffb6c1'];
